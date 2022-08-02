f95f147c7030b65494d79d1a62d0a0c00d95b33e
donghak <donghakhappy@gmail.com>
2022-08-03 00:22:26 +0900

    [BOJ] 1002 터렛 (원 교점)

--- Project4/taxi_geometry.cpp
+++ Project4/taxi_geometry.cpp
@@ -1,15 +1,36 @@
 #include <iostream>
-#define pi 3.14159265359
-
+#include <cstdlib>
+#include <cmath>
 using namespace std;
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
-	int r;
-	cin >> r;
-	cout << fixed;
-	cout.precision(6);
-	cout << r * r * pi << '\n' << (double)r * r * 2;
+	int n, i, k, x[6], distance, radius[2];
+	cin >> n;
+	for (k = 0; k < n; k++) {
+		for (i = 0; i < 6; i++) {
+			cin >> x[i];
+		}
+		distance = pow(x[0] - x[3], 2) + pow(x[1] - x[4], 2);
+		radius[0] = pow(x[2] + x[5], 2);
+		radius[1] = pow(x[2] - x[5], 2);
+		if (!distance && x[2] == x[5]) {
+			cout << -1;
+		}
+		else if (!distance) {
+			cout << 0;
+		}
+		else if (distance > radius[0] || distance < radius[1]) {
+			cout << 0;
+		}
+		else if (distance == radius[0] || distance == radius[1]) {
+			cout << 1;
+		}
+		else if (distance < radius[0] || distance > radius[1]) {
+			cout << 2;
+		}
+		cout << '\n';
+	}
 
 	return 0;
 }
