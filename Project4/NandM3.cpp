314e50c8cd76c1ce387af5caf00083c2c3e77802
donghak <donghakhappy@gmail.com>
2022-08-10 20:06:43 +0900

    [BOJ] 15651 N과M(3)

--- Project4/NandM3.cpp
+++ Project4/NandM3.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 int n, k;
-bool visited[9];
 int a[9];
 using namespace std;
-void dfs(int cnt,int st) {
+void dfs(int cnt) {
 	if (cnt == k) {
 		for (int i = 0; i < cnt; i++) {
 			cout << a[i] << ' ';
@@ -11,19 +10,16 @@ void dfs(int cnt,int st) {
 		cout << '\n';
 		return;
 	}
-	for (int i = st; i <= n; i++) {
-		if (visited[i]) continue;
-		visited[i] = true;
+	for (int i = 1; i <= n; i++) {
 		a[cnt] = i;
-		dfs(cnt + 1,i);
-		visited[i] = false;
+		dfs(cnt + 1);
 	}
 }
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 	cin >> n >> k;
-	dfs(0,1);
+	dfs(0);
 
 	return 0;
 }
